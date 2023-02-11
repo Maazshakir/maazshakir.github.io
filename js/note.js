5d71a2eb4ea0bcbcad4ab483cae15760dc073f6b
Maaz <46325822+Maazshakir@users.noreply.github.com>
2023-02-11 14:04:23 +0100

    removed default notes

--- js/note.js
+++ js/note.js
@@ -4,9 +4,6 @@ function notesFormat(Heading,mainText){
 }
 
 let allNotes = [
-    new notesFormat("Note1" , "sample note1"),
-    new notesFormat("Note2" , "sample note2"),
-    new notesFormat("Note3" , "sample note3")
 ]
 
 
